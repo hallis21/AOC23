09345a8b6d3cec7a2bda0b4823d79a2e5d9d8038
hallis21 <halvor_ring@hotmail.com>
2023-11-30 23:05:08 +0100

    single line

--- 1/1.py
+++ 1/1.py
@@ -14,43 +14,16 @@ tans = {
     "nine":9,
 }
 
-for l in lines:
-    first =''
-    last = ''
-    l1 = ''
-    l2 = ''
-
-    for leter in l:
-        try:
-            first = int(leter)
-            break
-        except:
-            l1 += leter
-            for t in tans:
-                if t in l1:
-                    first = tans[t]
-                    break
-            if first != '': break
-    print(first)
-  
-    rl = "".join([x for x in reversed(l)])
-    for i, leter in enumerate(reversed(l)):
-        try:
-            last = int(leter)
-            
-            break
-        except:
-            l2 = "".join([x for x in reversed(rl[:i+1].strip())])
-            print(l2, rl[i])
-            for t in tans:
-                if t in l2:
-
-                    last = tans[t]
-                    break
-            if last != '': break
-    print(last)
 
 
-    values.append(int(str(first)+str(last)))
-print(sum(values))
+# findNumber = lambda s: [n for n in [tans[s[x:]] if s[x:] in tans.keys() else False for x in range(len(s))] if n != False]
 
+
+# n1s = [[l[0] if type(l) == list else l for l in [number for number in [int(line[i]) if line[i].isnumeric() else (lambda s: [n for n in [{ "one": 1, "two": 2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9,}[s[x:]] if s[x:] in [ "one",  "two",  "three", "four", "five", "six", "seven", "eight", "nine"] else False for x in range(len(s))] if n != False])(line[:i+1]) for line in [l] for i in [n for n in range(len(l))]] if number != []]][0] for l in lines]
+# n2s = [[l[0] if type(l) == list else l for l in [number for number in [int(line[i]) if line[i].isnumeric() else (lambda s: [n for n in [{"one": 1,"two": 2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,}[s[x:]] if s[x:] in [ "one",  "two",  "three", "four", "five", "six", "seven", "eight", "nine"] else False for x in range(len(s))] if n != False])(line[:i+1]) for line in [l] for i in [n for n in range(len(l))]] if number != []]][-1] for l in lines]
+
+print(sum([int(str(x[0])+str(x[1])) for x in zip([[l[0] if type(l) == list else l for l in [number for number in [int(line[i]) if line[i].isnumeric() else (lambda s: [n for n in [{ "one": 1, "two": 2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9,}[s[x:]] if s[x:] in [ "one",  "two",  "three", "four", "five", "six", "seven", "eight", "nine"] else False for x in range(len(s))] if n != False])(line[:i+1]) for line in [l] for i in [n for n in range(len(l))]] if number != []]][0] for l in lines], [[l[0] if type(l) == list else l for l in [number for number in [int(line[i]) if line[i].isnumeric() else (lambda s: [n for n in [{"one": 1,"two": 2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,}[s[x:]] if s[x:] in [ "one",  "two",  "three", "four", "five", "six", "seven", "eight", "nine"] else False for x in range(len(s))] if n != False])(line[:i+1]) for line in [l] for i in [n for n in range(len(l))]] if number != []]][-1] for l in lines])]))
+
+
+
+# print([x for x in open("i.txt").readlines()])
